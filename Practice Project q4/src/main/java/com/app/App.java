85d42bf7162a8667ad06d052506b6266e2867241
hani <uhanir005@gmail.com>
2022-01-30 23:23:15 +0530

    commit

---
+++ Practice Project q4/src/main/java/com/app/App.java
@@ -0,0 +1,31 @@
+package com.app;
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+@WebServlet("/App")
+public class App extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		
+		String pId = request.getParameter("productId");
+		String pName = request.getParameter("productName");
+		String pPrice = request.getParameter("productPrice");
+		
+		HttpSession theSession = request.getSession();
+		
+		theSession.setAttribute("p_id", pId);
+		theSession.setAttribute("p_name", pName);
+		theSession.setAttribute("price", pPrice);
+		
+		response.sendRedirect("Display.jsp");
+		
+		
+	}
+
+}
